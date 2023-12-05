4b0589c1d7fd486cc0fd6be45780870bd5f0594c
Baptiste LEGRAS <baptiste.legras@epsi.fr>
2023-12-05 10:45:56 +0100

    Echauffement: OHCE

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def is_palindrome(word):
+    return word == word[::-1]
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Hello !")
 
+while True:
+    user_input = input("J'écoute ('exit' pour quitter) : ")
+
+    if user_input.lower() == 'exit':
+        print("Slt!")
+        break
+
+    if is_palindrome(user_input):
+        print("Bien dit!")
+    else:
+        mirrored_text = user_input[::-1]
+        print(f"{mirrored_text}")
